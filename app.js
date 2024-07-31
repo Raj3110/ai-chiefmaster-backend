c0b2f1a2cb0bea1c02c67c3f623838bd6655780d
Raj Pawar <77847033+Raj3110@users.noreply.github.com>
2024-08-01 00:53:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URI,
+    origin: "https://aichiefmaster.netlify.app/",
     credentials: true,
   })
 );
